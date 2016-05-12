0c82919e4c82fde7bde71c9800f4a1aaf264a2fe
Alexander Bauer <sasha@linux.com>
2016-05-12 15:47:13 -0400

    Add first-pass kNN

--- Clustering.py
+++ Clustering.py
@@ -24,6 +24,39 @@ def knn(vectors, k, positions=None):
         # Choose the initial positions inside the box of extreme points.
         positions = minvec + np.random.rand(k, dimensionality) * spread
 
+    while True:
+        positions_old = positions.copy()
+
+        # Keep space for the points to be classified into.
+        classes = [[]] * k
+
+        # For each vector, find the nearest proposed center.
+        for point in vectors:
+            # Compute the distance from the point to each center.
+            distances = np.linalg.norm(positions - point, axis = 1)
+            # Find the index of the nearest point.
+            closest_index = distances.argmin()
+
+            classes[closest_index].append(point)
+
+        # For each class that has any points, recompute the proposed center.
+        for i, points in enumerate(classes):
+            if len(points) == 0:
+                # If there are no points in this class, leave the center as-is.
+                continue
+            elif len(points) == 1:
+                # If there is only one point, use it.
+                positions[i] = points[0]
+            else:
+                # Compute a new vector that is the center of the given points.
+                positions[i] = np.concatenate(points).mean(axis = 0)
+
+
+        # Finally, check if the positions haven't changed. If so, they are the true
+        # centers. Otherwise, iterate again with positions set.
+        if np.equal(positions, positions_old).all():
+            return positions
+
 def read_vectors(f, vector_type=float, sep=" ", include_empty=False):
     """Read vectors from an open file stream. Vectors must be listed one per
     line, and be separated with the `sep` character. If `include_empty` is set,
@@ -32,7 +65,9 @@ def read_vectors(f, vector_type=float, sep=" ", include_empty=False):
     Returns an iterable, not necessarily a list.
     """
     for line in f:
-        fields = [vector_type(entry) for entry in line.split(sep)]
+        fields = [vector_type(entry) for entry
+                in line.strip().split(sep)
+                if entry] # Skip falsy entries
 
         # Skip empty lines if directed to.
         if len(fields) == 0 and not include_empty:
